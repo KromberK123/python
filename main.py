9d1de69a83a1a1ddccf7493ba8d21a0d09c20576
KromberK123 <114129840+KromberK123@users.noreply.github.com>
2022-09-22 12:00:25 +0700

    Первый урок
    у меня 4 легендарных бойца в Brawl Stars

---
+++ main.py
@@ -0,0 +1,33 @@
+# print("Hello world")
+# message_1 = input("Сообщение 1:")
+# print(type(message_1))
+# name = input("Твое Имя?")
+# age = input("Твой возвраст?")
+# time = input("Текущее время?")
+# print(f"Меня зовут {name}, мне {age} лет. И сейчас {time}!")
+# a = int(input("a:"))
+# b = int(input("b:"))
+# p = (a+b) * 2
+# s = a * b
+#
+# print("P=", p)
+# print("S=", s)
+# a = input("a: ")
+# b = input("b: ")
+#
+#
+# c = a
+# a = b
+# b = c
+# print("a=", a)
+# print ("b=", b)
+# a = int(input("a: "))
+# b = int(input("b: "))
+#
+# print((a+b)/2)
+
+# hours = int(input("h: "))
+# minutes = int(input("m: "))
+# seconds = int(input("s: "))
+#
+# print (hours * 60 * 60 + minutes * 60 + seconds)
