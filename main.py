7388aa2202aae91c082eb5ea3ec6b590316f6ea5
KromberK123 <a37507687@gmail.com>
2022-09-29 12:04:17 +0700

    Прошёл второй урок в Movavi

--- main.py
+++ main.py
@@ -1,33 +1,38 @@
-# print("Hello world")
-# message_1 = input("Сообщение 1:")
-# print(type(message_1))
-# name = input("Твое Имя?")
-# age = input("Твой возвраст?")
-# time = input("Текущее время?")
-# print(f"Меня зовут {name}, мне {age} лет. И сейчас {time}!")
-# a = int(input("a:"))
-# b = int(input("b:"))
-# p = (a+b) * 2
-# s = a * b
-#
-# print("P=", p)
-# print("S=", s)
-# a = input("a: ")
-# b = input("b: ")
-#
-#
-# c = a
-# a = b
-# b = c
-# print("a=", a)
-# print ("b=", b)
-# a = int(input("a: "))
-# b = int(input("b: "))
-#
-# print((a+b)/2)
 
-# hours = int(input("h: "))
-# minutes = int(input("m: "))
-# seconds = int(input("s: "))
-#
-# print (hours * 60 * 60 + minutes * 60 + seconds)
+# print("Последняя цифра", a % 10)
+# print("Первая цифра", a//100)
+# print("Вторая цифра", a //1a =int(input("a: "))
+# a =int(input("a: "))
+# first = a // 100
+# second = a // 10 % 10
+# tretie = a % 10
+# print("Последняя цифра", a % 10)
+# print("Первая цифра", a//100)
+# print("Вторая цифра", a // 10 % 10)
+# print("Произведение чисел =", first * second * tretie)
+# a =int(input("a:"))
+# if a % 2 == 1:
+#     a = a + 1
+# else:
+#      a = a + 2
+# print(a)
+# d =int(input("d: "))
+# print(d * 3.14)
+# d = int(input("d: "))
+# r = d/2
+# print("Длина = ", d * 3.14)
+# print("Площадь = ", 3.14 * r ** 2)
+# sec = int(input("sec: "))
+# h = sec // 3600
+# m = sec % 3600 // 60
+# s = sec % 3600 % 60 % 60
+# print(h)
+# print(m)
+# print(s)
+# if h < 10:
+#     h = "0" + str(h)
+# if m < 10:
+#     m = "0" + str(m)
+# if s < 10:
+#     s = "0" + str(s)
+# print(f"{h}:{m}:{s}")
